6f6bc681f62ebc1c743d526642e8b8dd2cceb015
Keitarou0727 <bzfan.keitarou@gmail.com>
2025-10-28 21:51:17 +0900

    kadai_016をアップロード

---
+++ kadai_016/class.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>16章課題</title>
+</head>
+<body>
+<p>
+
+<?php
+class Food {
+    public function __construct(string $name, int $price) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+    
+    public function show_price() {
+        return $this->price;
+    }
+}
+
+class Animal {
+    public function __construct(string $name, int $height, int $weight) {
+        $this->name = $name;
+        $this->height = $height;
+        $this->weight = $weight;
+    }
+    
+    public function show_height() {
+        return $this->height;
+    }
+}
+
+$apple = new Food('リンゴ', 10);
+$cat = new Animal('マロン', 20, 30);
+
+echo $apple->show_price();
+echo '<br>';
+echo $cat->show_height();
+
+print_r($apple);
+echo '<br>';
+print_r($cat);
+echo '<br>';
+
+print_r($apple->show_price());
+echo '<br>';
+print_r($cat->show_height());
+echo '<br>';
+?>
+</p>
+
+</body>
+</html>
